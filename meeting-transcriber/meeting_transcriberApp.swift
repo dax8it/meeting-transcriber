841ae151af27223ed146e472db5527f6c871f192
dax8it <alexcovo@gmail.com>
2026-01-10 22:42:49 -0500

    Initial Commit

---
+++ meeting-transcriber/meeting_transcriberApp.swift
@@ -0,0 +1,17 @@
+//
+//  meeting_transcriberApp.swift
+//  meeting-transcriber
+//
+//  Created by Alex Covo on 1/10/26.
+//
+
+import SwiftUI
+
+@main
+struct meeting_transcriberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
